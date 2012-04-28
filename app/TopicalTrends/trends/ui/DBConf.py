bd05c702aeddf4445ddaacbf34d24bf7c5370788
Ovo Dibie <dibieo@gmail.com>
2012-04-28 14:54:35 -0600

    Saving changes after merge

--- app/TopicalTrends/trends/ui/DBConf.py
+++ app/TopicalTrends/trends/ui/DBConf.py
@@ -8,14 +8,7 @@ class DBConf:
     
     # SQL connection info
     host = 'localhost'
-<<<<<<< HEAD
     user = 'root'
     passwrd = 'america'
     db = 'topicaltrends'
     
-=======
-    user = 'tt'
-    passwrd = 'tt123'
-    db = 'tt'
-    
->>>>>>> 3bd3aca969e04d27d75b8cddf9a1833161719f86
