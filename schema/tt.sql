b323843e65d4d50660c3b37c4ff62977c2b6caaf
Houman Farokhzad <hfarrokhzad1987@gmail.com>
2012-05-01 02:34:44 -0600

    database updated

--- schema/tt.sql
+++ schema/tt.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 29, 2012 at 10:56 PM
+-- Generation Time: May 01, 2012 at 02:31 AM
 -- Server version: 5.1.62
 -- PHP Version: 5.3.6-13ubuntu3.6
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `feed` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'insertion date',
   PRIMARY KEY (`id`),
   UNIQUE KEY `link_hash` (`link_hash`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1402 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=584 ;
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `feeditem` (
   UNIQUE KEY `guid_hash` (`title_hash`),
   UNIQUE KEY `link_hash` (`link_hash`),
   KEY `feed_id` (`feed_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6052 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `feeditem_tag` (
   PRIMARY KEY (`id`),
   KEY `feeditem_id` (`feeditem_id`),
   KEY `tag_id` (`tag_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25349 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'insertion date',
   `id_miner` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10143 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Constraints for dumped tables
@@ -127,8 +127,8 @@ ALTER TABLE `feeditem`
 -- Constraints for table `feeditem_tag`
 --
 ALTER TABLE `feeditem_tag`
-  ADD CONSTRAINT `feeditem_tag_ibfk_1` FOREIGN KEY (`feeditem_id`) REFERENCES `feeditem` (`id`),
-  ADD CONSTRAINT `feeditem_tag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
+  ADD CONSTRAINT `feeditem_tag_ibfk_4` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`),
+  ADD CONSTRAINT `feeditem_tag_ibfk_3` FOREIGN KEY (`feeditem_id`) REFERENCES `feeditem` (`id`);
 
 --
 -- Constraints for table `feed_tag`
